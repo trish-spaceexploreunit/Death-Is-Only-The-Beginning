10ffaec62067806639c170a2cc2f4bf4f41a1dbf
trish-spaceexploreunit <trishag1397@gmail.com>
2022-05-21 15:10:16 -0400

    basic character controls

---
+++ Death Is Only The Beginning/Assets/Scripts/CultistController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultistController : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+    [SerializeField] float jumpHeight = 0.5f;
+    bool inAir;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) & !inAir)
+        {
+            inAir = true;
+            transform.Translate(0, jumpHeight, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            //There may well be other things we want to check here eventually
+            if (inAir)
+            {
+                inAir = false;
+            }
+        }
+    }
+
+    
+}
