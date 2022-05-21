3767bfe1e2ac51de7b4bb178567264c577d8d723
trish-spaceexploreunit <trishag1397@gmail.com>
2022-05-21 15:23:22 -0400

    tuners for controls

--- Death Is Only The Beginning/Assets/Scripts/CultistController.cs
+++ Death Is Only The Beginning/Assets/Scripts/CultistController.cs
@@ -6,17 +6,20 @@ public class CultistController : MonoBehaviour
 {
     [SerializeField] float speed = 0.5f;
     [SerializeField] float jumpHeight = 0.5f;
+    [SerializeField] float fallSpeed = -0.1f;
     bool inAir;
     
     void Update()
     {
+        float frameAdjustedSpeed = speed * Time.deltaTime;
+        float frameAdjustedFallSpeed = fallSpeed * Time.deltaTime;
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(speed, 0, 0);
+            transform.Translate(frameAdjustedSpeed, inAir? frameAdjustedFallSpeed:0, 0);
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-speed, 0, 0);
+            transform.Translate(-frameAdjustedSpeed, inAir? frameAdjustedFallSpeed:0, 0);
         }
         if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) & !inAir)
         {
