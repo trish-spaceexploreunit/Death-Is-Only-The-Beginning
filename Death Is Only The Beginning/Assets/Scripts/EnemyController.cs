d621d7b28b021159547a3942900b06b71ac0df07
trish-spaceexploreunit <trishag1397@gmail.com>
2022-05-25 12:32:38 -0400

    adding enemy logic

---
+++ Death Is Only The Beginning/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] bool moveLeft;
+    [SerializeField] float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float frameAdjustedMovementSpeed = movementSpeed * Time.deltaTime;
+        transform.Translate(moveLeft ? -frameAdjustedMovementSpeed : frameAdjustedMovementSpeed, 0, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            moveLeft = !moveLeft;
+        }
+    }
+}
