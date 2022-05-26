709c2e5b3ed6e5e19da8af4ce56f426ce6297924
QueenPercy <persephonerumsey@gmail.com>
2022-05-26 13:35:10 -0400

    Button scripts

---
+++ Death Is Only The Beginning/Assets/Scripts/StartBtns.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartBtns : MonoBehaviour
+{
+    public Button begin, credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn1 = begin.GetComponent<Button>();
+        Button btn2 = credits.GetComponent<Button>();
+
+        btn1.onClick.AddListener(beginTask);
+        btn2.onClick.AddListener(creditsTask);
+    }
+
+    void beginTask()
+    {
+        SceneManager.LoadScene("LevelExperimentation");
+    }
+
+    void creditsTask()
+    {
+        Debug.Log("Credits go here");
+    }
+}
